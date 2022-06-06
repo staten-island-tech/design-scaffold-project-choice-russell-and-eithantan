422724e3c60aaaea14cf523b8b8ac1b4a3395bef
Eitan Halevi <ethan13@nycstudents.net>
2022-06-06 10:07:10 -0400

    asdfghjkl;

--- vite.config.js
+++ vite.config.js
@@ -10,17 +10,14 @@ module.exports = defineConfig({
                 nested: resolve(__dirname, "./html/cocoa.html"),
                 nested2: resolve(__dirname, "./html/8up.html"),
                 nested3: resolve(__dirname, "./html/dew.html"),
-                nested4: resolve(__dirname, "./html/download.html"),
                 nested5: resolve(__dirname, "./html/DrSalt.html"),
                 nested6: resolve(__dirname, "./html/foontis.html"),
                 nested7: resolve(__dirname, "./html/pepes.html"),
                 nested8: resolve(__dirname, "./html/pink.html"),
                 nested9: resolve(__dirname, "./html/root.html"),
                 nested10: resolve(__dirname, "./html/spritz.html"),
-                nested11: resolve(__dirname, "./html/swoosh.html"),
                 nested12: resolve(__dirname, "./html/watah.html"),
-
-
+                
             }
         }
     }
